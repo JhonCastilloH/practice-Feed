96723604a40261fd87d4209079c170852619fcbd
John Castillo <profe.johncastillo@gmail.com>
2022-08-03 20:26:07 +0800

    Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
