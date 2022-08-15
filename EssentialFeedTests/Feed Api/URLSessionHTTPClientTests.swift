e2ad9e8d29edd103b8285c6359177609fecb764c
John Castillo <profe.johncastillo@gmail.com>
2022-08-16 00:52:53 +0800

    Move start/stop intercepting URLRequest to setup/teardown

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -49,8 +49,18 @@ class URLSessionHTTPClientTests: XCTestCase {
 //        XCTAssertEqual(task.resumeCallCount, 1)
 //    }
     
-    func test_getFromURL_performsGETRequestWithURL() {
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInteceptingRequests()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
+        
         let url = URL(string: "http://any-url.com")!
         
         let exp = expectation(description: "Wait for completion")
@@ -64,11 +74,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLSessionHTTPClient().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInteceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
         
         let error = NSError(domain: "any error", code: 1)
@@ -91,7 +99,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInteceptingRequests()
     }
     
     // URLProtocol
