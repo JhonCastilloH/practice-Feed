95acc4ac61f343481e48756bd7966ce6b8bb2d3e
John Castillo <profe.johncastillo@gmail.com>
2022-09-05 17:49:02 +0800

    Does not delete cache upon creation

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCase.swift
+//  EssentialFeedTests
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCase: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
