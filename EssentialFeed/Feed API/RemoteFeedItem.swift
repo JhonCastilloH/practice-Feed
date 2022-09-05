509863f026647fac0c6943a412b2a2a371322938
John Castillo <profe.johncastillo@gmail.com>
2022-09-06 00:42:15 +0800

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
