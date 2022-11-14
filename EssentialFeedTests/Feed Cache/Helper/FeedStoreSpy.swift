fe996fd4e353deb897e5cce49397ebde6ac76330
John Castillo <profe.johncastillo@gmail.com>
2022-11-14 17:02:13 +0800

    Load command requests cache retrieval

--- EssentialFeedTests/Feed Cache/Helper/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helper/FeedStoreSpy.swift
@@ -11,6 +11,7 @@ internal class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -42,4 +43,9 @@ internal class FeedStoreSpy: FeedStore {
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletions[index](nil)
     }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
+    }
+    
 }
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -13,6 +13,14 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [])
     }
+    
+    func test_load_requestCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 
     // MARK: - Helpers
     
