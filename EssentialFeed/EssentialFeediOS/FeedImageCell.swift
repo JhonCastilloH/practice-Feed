b13a367415b34a7f8ae6837d7a16339fd0d6be6e
John Castillo <profe.johncastillo@gmail.com>
2023-02-01 01:31:32 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
